4895b80e36d3f08257ff9e60b995665b5b603c30
Gunnar Holwerda <gunnarholwerda@gmail.com>
2014-11-21 09:34:57 -0700

    Fixes footer so it moves to bottom of page dynamically, adds new picture to about-us page and updates original picture, comments javascript

--- Web Design/Final Project/js/menu.js
+++ Web Design/Final Project/js/menu.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){
+	//Click on drinks updates menu
 	$('#left-item').click(function(){
 		$('#title').text("Drinks");
 		$('#menu').html("<b>There are so many drinks here.</b>");
 	});
 
+	//Click on appetizers updates menu
 	$('#appetizers').click(function(){
 		$('#title').text("Appetizers");
 		$('#menu').html("<b>There are so many appetizers here.</b>");
 	});
 
+	//Click on salads updates menu
 	$('#salads').click(function(){
 		$('#title').text("Salads");
 		$('#menu').html("<b>There are so many salads here.</b>");
 	});
 
+	//Click on entrees updates menu
 	$('#entrees').click(function(){
 		$('#title').text("Entrees");
 		$('#menu').html("<b>There are so many entrees here.</b>");
 	});
 
+	//Click on desserts updatesmenu
 	$('#right-item').click(function(){
 		$('#title').text("Desserts");
 		$('#menu').html("<b>There are so many desserts here.</b>");
