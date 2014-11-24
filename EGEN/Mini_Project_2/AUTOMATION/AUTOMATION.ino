04176830cfdbab6efd985b555f7b3e5a97b3f6fe
Gunnar Holwerda <gunnarholwerda@gmail.com>
2014-11-23 17:50:06 -0700

    Updates salinity measurement sketch, test_sketch now controls solenoid

--- EGEN/Mini_Project_2/AUTOMATION/AUTOMATION.ino
+++ EGEN/Mini_Project_2/AUTOMATION/AUTOMATION.ino
@@ -4,12 +4,14 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <Servo.h> 
+
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *FAN = AFMS.getMotor(3);//Fan in Motor Port 3
 Adafruit_DCMotor *SOLENOID = AFMS.getMotor(2);//Solenoid in Motor Port 2
+
 void setup() {
   Serial.begin(9600);// set up Serial library at 9600 bps
- AFMS.begin();//Create with the default frequency 1.6KHz
+  AFMS.begin();//Create with the default frequency 1.6KHz
 }
 
 //--Time Set-Up--//
@@ -18,6 +20,7 @@ unsigned long Timer_Servo;
 
 //---Magnetic Stir Bar---//
 void loop() {
+ 
  FAN->run(FORWARD);//Set to FORWARD/BACKWARDif FAN does not run
  FAN->setSpeed(255);  
  
@@ -28,19 +31,20 @@ void loop() {
  SOLENOID->run(RELEASE);
 
  //---Hopper Gate Control---//
- Servo SERVO;  // create servo object to control a servo
-int pos = 0;    // variable to store the servo position 
-  SERVO.attach(9);  // attaches the servo on pin 9 to the servo object  
-  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    SERVO.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    SERVO.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
+ Servo SERVO9, SERVO10;  // create servo object to control a servo
+ int pos = 0;    // variable to store the servo position 
+ SERVO9.attach(9);  // attaches the servo on pin 9 to the servo object 
+ SERVO10.attach(10);
+ for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+ {                                  // in steps of 1 degree 
+   SERVO9.write(pos);              // tell servo to go to position in variable 'pos' 
+   delay(15);                       // waits 15ms for the servo to reach the position 
+ } 
+ for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+ {                                
+   SERVO9.write(pos);              // tell servo to go to position in variable 'pos' 
+   delay(15);                       // waits 15ms for the servo to reach the position 
+ } 
 } 
 
 
--- EGEN/Mini_Project_2/test_sketch/test_sketch.ino
+++ EGEN/Mini_Project_2/test_sketch/test_sketch.ino
@@ -1,11 +1,49 @@
-int pin = 13;
+//---Library Callout---//
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+#include <Servo.h> 
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *FAN = AFMS.getMotor(3);//Fan in Motor Port 3
+Adafruit_DCMotor *SOLENOID = AFMS.getMotor(2);//Solenoid in Motor Port 2
+
+int servoButtonPin = 3, solenoidButtonPin = 2;
+int servoButtonState = 0, solenoidButtonState = 0;
+int solenoidTime = 500;
+boolean notRan = true;
 
 void setup(){
-  pinMode(pin, OUTPUT);
+  pinMode(servoButtonPin, INPUT);
+  pinMode(solenoidButtonPin, INPUT);
   Serial.begin(9600);
+  AFMS.begin();
 }
 
+//--Time Set-Up--//
+unsigned long Timer_Solenoid;
+unsigned long Timer_Servo;
+
 void loop() {
-  Serial.print(F("What is going on here?\n"));
-  delay(1000);
+  delay(500);
+  
+  FAN->run(FORWARD);//Set to FORWARD/BACKWARDif FAN does not run
+  FAN->setSpeed(255); 
+  
+  //servoButtonState = digitalRead(servoButtonPin);
+  solenoidButtonState = digitalRead(solenoidButtonPin);
+  if (solenoidButtonState == HIGH && notRan) {
+    //The solenoid button was pressed
+    Serial.println("The solenoid button is on");
+    //---Upper Reservoir Solenoid Valve---//
+    SOLENOID->run(FORWARD);//Set to FORWARD/BACKWARD if SOLENOID does not run
+    SOLENOID->setSpeed(255);
+    delay(solenoidTime);
+    SOLENOID->run(RELEASE);
+    notRan = false;
+  }
+  else {
+    Serial.println("The solenoid button is off");
+    notRan = true;
+  }
 }
