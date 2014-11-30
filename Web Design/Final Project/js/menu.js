c6da2456ab850152498043ee282c250918860f8e
Gunnar Holwerda <gunnarholwerda@gmail.com>
2014-11-30 15:32:08 -0700

    Finished menu for the menu page

--- Web Design/Final Project/js/menu.js
+++ Web Design/Final Project/js/menu.js
@@ -8,24 +8,24 @@ $(document).ready(function(){
 	//Click on appetizers updates menu
 	$('#appetizers').click(function(){
 		$('#title').text("Appetizers");
-		$('#menu').html("There are so many appetizers here.");
+		$('#menu').html("<div id=\"left-menu\" class=\"menu-box\">\n\t<h3>Stuff</h3>\n<ul>\n\t<li>Mozzarella Sticks</li>\n\t<li>Calamari</li>\n\t<li>Cheese Platter</li>\n\t<li>Starter Pizza</li>\n</ul>\n</div>\n<div id=\"right-menu\" class=\"menu-box\">\n\t<h3>Other Stuff</h3>\n<ul>\n\t<li>Starter Salad</li>\n\t<li>Appetizer Spaghetti</li>\n\t<li>Grande Appetizer</li>\n\t<li>Sample Platter</li>\n</ul>\n</div>\n");
 	});
 
 	//Click on salads updates menu
 	$('#salads').click(function(){
 		$('#title').text("Salads");
-		$('#menu').html("There are so many salads here.");
+		$('#menu').html("<div id=\"left-menu\" class=\"menu-box\">\n\t<h3>Salads</h3>\n<ul>\n\t<li>Caesar</li>\n\t<li>Potato</li>\n\t<li>Yummy</li>\n</ul>\n</div>\n<div id=\"right-menu\" class=\"menu-box\">\n\t<h3>Dressings</h3>\n<ul>\n\t<li>Ranch</li>\n\t<li>Dressing 2</li>\n\t<li>Dressing 3</li>\n\t<li>Dressing 4</li>\n</ul>\n</div>\n");
 	});
 
 	//Click on entrees updates menu
 	$('#entrees').click(function(){
 		$('#title').text("Entrees");
-		$('#menu').html("There are so many entrees here.");
+		$('#menu').html("<div id=\"left-menu\" class=\"menu-box\">\n\t<h3>Pasta</h3>\n<ul>\n\t<li>Spaghetti</li>\n\t<li>Ravioli</li>\n\t<li>Super yummy pasta delight</li>\n</ul>\n</div>\n<div id=\"right-menu\" class=\"menu-box\">\n\t<h3>Pizza</h3>\n<ul>\n\t<li>Margherita</li>\n\t<li>Meatball Delight</li>\n\t<li>Vegan Extravaganza</li>\n\t<li>Bellion Bell Pepper</li>\n</ul>\n</div>\n");
 	});
 
 	//Click on desserts updatesmenu
 	$('#right-item').click(function(){
 		$('#title').text("Desserts");
-		$('#menu').html("There are so many desserts here.");
+		$('#menu').html("<div id=\"left-menu\" class=\"menu-box\">\n\t<h3>Cheesecakes</h3>\n<ul>\n\t<li>Peanut Butter Cheesecake</li>\n\t<li>Cheesy Cheesecake</li>\n\t<li>Raspberry Cheesecake</li>\n</ul>\n</div>\n<div id=\"right-menu\" class=\"menu-box\">\n\t<h3>Gellato</h3>\n<ul>\n\t<li>Chocolate</li>\n\t<li>Vanilla</li>\n\t<li>Strawberry Delight</li>\n\t<li>Pistachio</li>\n</ul>\n</div>\n");
 	});
 })
