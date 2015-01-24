50363c9ac79066d470ef4cba4386927b9c056a63
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-01-24 16:31:11 -0700

    Creates a dummy main.c to for a brute force tool

---
+++ Computer Security/Brute Force Tool/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<unistd.h> //Include the getopt library see: http://www.gnu.org/software/libc/manual/html_node/Using-Getopt.html#Using-Getopt
+
+int main(int argc, char *argv[])
+{
+    	/*
+    	We need to get the options here tthat include:
+    	single password
+    	password file
+    	single user
+    	user file
+    	IP Address 
+
+	hacked = 0;
+	Figure out how to open port 22 on a server see: http://en.wikibooks.org/wiki/C_Programming/Networking_in_UNIX
+	while(!hacked)
+	{
+		Then figure out how to send the user option as the login
+		And figure out how to send the password
+
+		Get the response
+		if (response == success) 
+		{
+			Then we have done it, we print the user and pass that was found to work and how many tries it took
+			hacked = 1;
+		}
+		else if (response ==  error)
+		{
+			Print the error message
+		}
+		else
+		{
+			We failed so go back to top of loop
+		}
+	}
+	*/
+
+    	return 0;
+}
