4859c439179b762979eace2cf17b893353575200
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-02-15 19:55:35 -0700

    Stop words are now excluded from bigram counts

--- Programming Languages/Holwerda.Gunnar.lab1.pl
+++ Programming Languages/Holwerda.Gunnar.lab1.pl
@@ -26,6 +26,10 @@ my @special_chars = (
 );
 my @punctuation
     = ( "\\?", "¿", "!", "¡", "\\.", ";", "&", "\$", "@", "%", "#", "|" );
+my @stop_words = (
+    "a",  "an", "and", "the", "with", "from", "for", "out",
+    "by", "in", "of",  "on",  "or",   "to"
+);
 %word_hash = ();
 
 # This loops through each line of the file
@@ -37,9 +41,19 @@ while ( my $line = <INFILE> ) {
     # Sets the title to all lower case
     $line = lc $line;
 
-# Iterates over array of special characters and removes all text following them
+    # Iterates over array of special characters and removes all text following
     foreach $char (@special_chars) {
         $line =~ s/($char.*)//;
+    }
+
+    # Trim beginning and ending whitespace from string
+    $line =~ s/^\s+|\s+$//g;
+
+    # Iterate over the stop words and remove them from the song title
+    foreach $stop_word (@stop_words) {
+
+        # Remove the word
+        $line =~ s/( $stop_word )//;
     }
 
     # Iterates over array of punctuation and removes all of them
@@ -55,14 +69,20 @@ while ( my $line = <INFILE> ) {
         @words = split( ' ', $line );
 
         for ( $i = 0; $i < @words; $i++ ) {
-            if ( ( $i + 1 ) < @words ) {
 
-      # Create hash of words for each word and add 1 to the count of that word
-                $word_hash{ $words[$i] }{ $words[ $i + 1 ] } += 1;
+            # Don't count the stop words
+            if (    !grep( /^$words[$i]$/, @stop_words )
+                and !grep( /^$words[$i + 1]$/, @stop_words ) )
+            {
+                if ( ( $i + 1 ) < @words ) {
+
+                   # Create hash of words for each word and add 1 to the count
+                    $word_hash{ $words[$i] }{ $words[ $i + 1 ] } += 1;
+                }
+
+                # This increases count of current word
+                $word_hash{ $words[$i] }{'_'} += 1;
             }
-
-            # This increases count of current word
-            $word_hash{ $words[$i] }{'count'} += 1;
         }
 
     }
@@ -71,44 +91,39 @@ while ( my $line = <INFILE> ) {
 # Close the file handle
 close(INFILE);
 
-# foreach $word ( keys %word_hash ) {
-#     $distinct_word_count = ( keys $word_hash{$word} ) - 1;
-#     print "$word: $word_hash{$word}{'count'} $distinct_word_count\n";
-#     foreach $following_word (
-#         sort { $word_hash{$word}{$b} <=> $word_hash{$word}{$a} }
-#         keys %{ $word_hash{$word} }
-#         )
-#     {
-#         if ( $following_word ne "count" ) {
-#             print
-#                 "\t$following_word; count = $word_hash{$word}{$following_word}\n";
-#         }
-#     }
-# }
+print_hash();
 
 print "File parsed. Bigram model built.\n\n";
 
 # User control loop
-while ( $input ne "q" ) {
-    print "Enter a word [Enter 'q' to quit]: ";
-    $input = <STDIN>;
-    chomp($input);
-    print "\n";
+# while ( $input ne "q" ) {
+#     print "Enter a word [Enter 'q' to quit]: ";
+#     $input = <STDIN>;
+#     chomp($input);
+#     print "\n";
 
-    mcw($input);
-}
+#     if ($input == "q") {
+#     	exit;
+#     }
+
+#     mcw($input);
+# }
 
 sub mcw {
 
     # Get total number of arguments passed
-    $argument = $_[0];
+    my $argument = $_[0];
 
     for ( $i = 0; $i < 20; $i++ ) {
         print "$argument ";
-        $key = (
+
+        # Get the second element from the array because the first will always
+        # be the number of times the word appears
+        my $key = (
             sort { $word_hash{$argument}{$b} <=> $word_hash{$argument}{$a} }
                 keys %{ $word_hash{$argument} }
         )[1];
+
         # Need to add in random decider for ties in word count
         # Need to add in the removal of Stop Words from the song title
         # Need to add in something that stops it from revolving forever
@@ -116,4 +131,21 @@ sub mcw {
         $argument = $key;
     }
     print "\n";
+}
+
+sub print_hash {
+    foreach $word ( keys %word_hash ) {
+        $distinct_word_count = ( keys $word_hash{$word} ) - 1;
+        print "$word: $word_hash{$word}{'_'} $distinct_word_count\n";
+        foreach $following_word (
+            sort { $word_hash{$word}{$b} <=> $word_hash{$word}{$a} }
+            keys %{ $word_hash{$word} }
+            )
+        {
+            if ( $following_word ne "_" ) {
+                print
+                    "\t$following_word; count = $word_hash{$word}{$following_word}\n";
+            }
+        }
+    }
 }
