83f7445f3e8928860ca2c55aaac48920f94c10e9
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-04-08 10:28:41 -0600

    Adds txt file for answers to lab questions, includes the warmup part of the lab in the .scm file

--- Programming Languages/schemeLab/holwerda_gunnar.lab3.scm
+++ Programming Languages/schemeLab/holwerda_gunnar.lab3.scm
@@ -1,1 +1,13 @@
-(display "Hello World!\n")
+; Warmup
+
+(define (f lst)
+	; (a) ;
+	(if (null? lst)
+		; (b) ;
+		'()
+		; (c) ;
+		(cons (+ 1 (car lst)) (f (cdr lst)))
+	)
+)
+
+(display (f '(3 1 4 1 5 9)))
