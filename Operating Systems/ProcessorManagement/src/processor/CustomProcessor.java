0f97ef4bb059c7b1a3654bd192853a9e3d1c6b15
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-09-13 12:06:48 -0600

    Reworks CustomProcessor run method

--- Operating Systems/ProcessorManagement/src/processor/CustomProcessor.java
+++ Operating Systems/ProcessorManagement/src/processor/CustomProcessor.java
@@ -8,6 +8,7 @@ package processor;
 import processor.ProcessorRoundRobin;
 import processor.Processor;
 import static java.lang.Thread.sleep;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,22 +25,17 @@ public class CustomProcessor extends Processor {
     @Override
     public void run() {
         while(!this.jobList.isEmpty()) {
-            for (int i = this.jobList.size() - 1; i >= 0; i--) {
-                //System.out.println(this.name + " is still running with " + this.jobList.size() + " jobs");
-                try {
-                    // Sleep for 1ms to simulate the job running
-                    sleep(1);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ProcessorRoundRobin.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                Job job = this.jobList.get(i);
-                // Subtract 1 from the sleep counter of the job
-                job.sleepTime -= 1;
-                
-                // If the job has no computing time left, remove it from the list
-                if (job.sleepTime == 0) {
-                    this.removeJob(job);
-                }
+            Job job = this.jobList.get(0);
+            try {
+                // Sleep for 1ms to simulate the job running
+                sleep(1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProcessorRoundRobin.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            job.sleepTime -= 1;
+            
+            if (job.sleepTime == 0) {
+                this.removeJob(job);
             }
         }
     }
--- Operating Systems/ProcessorManagement/src/processor/Job.java
+++ Operating Systems/ProcessorManagement/src/processor/Job.java
@@ -4,10 +4,24 @@ package processor;
  *
  * @author Gunnar
  */
-public class Job {
+public class Job implements Comparable{
     public int sleepTime;
     
     public Job(int sleepTime) {
         this.sleepTime = sleepTime;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Job comparator = (Job) o;
+        if (this.sleepTime > comparator.sleepTime) {
+            return 1;
+        }
+        else if (this.sleepTime == comparator.sleepTime) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 }
