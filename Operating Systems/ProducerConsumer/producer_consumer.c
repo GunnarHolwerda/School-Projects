3478f16bcb36f227ec754006eae8fc8f8c2841da
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-10-06 19:52:55 -0600

    Mutex locking now working. Still get segmentation fault somewhere

--- Operating Systems/ProducerConsumer/producer_consumer.c
+++ Operating Systems/ProducerConsumer/producer_consumer.c
@@ -11,7 +11,8 @@ struct Node {
 };
 
 struct Node* head = NULL;
-int numberOfNodes = 0;
+static int numberOfNodes = 0;
+pthread_mutex_t mutex;
 
 // Creates a new node and returns it
 struct Node* createNewNode(int val) {
@@ -21,6 +22,15 @@ struct Node* createNewNode(int val) {
   newNode->prev = NULL;
 
   return newNode;
+}
+
+void printList() {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->value);
+        temp = temp->next;
+    }
+    printf("\n");
 }
 
 // Insert a node at the end of the linked list
@@ -58,6 +68,8 @@ void removeNodeFromHead(int removeEven) {
             // Remove node by setting previous nodes next to
             // current nodes next
             temp->prev->next = temp->next;
+            free(temp);
+            printList();
             numberOfNodes--;
             return;
         }
@@ -73,19 +85,10 @@ void generateInitialValues() {
     }
 }
 
-void printList() {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d -> ", temp->value);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-void *Processor(void* genEven) {
+void *Producer(void* genEven) {
     int randNum;
     int generateEvenNumbers = (long) genEven;
-    int numCycles = 50;
+    int numCycles = 500;
     int curCycle;
     for (curCycle = 0; curCycle < numCycles; curCycle++) {
         // Generate a random odd number or even number
@@ -103,39 +106,50 @@ void *Processor(void* genEven) {
         }
 
         if (numberOfNodes < 20) {
+            pthread_mutex_lock(&mutex);
+
+            puts("Producer");
             insertNodeAtTail(randNum);
             printf("Inserting %d into linked list, list has %d nodes\n",
                 randNum,
                 numberOfNodes
             );
             printList();
-            sleep(3);
+            pthread_mutex_unlock(&mutex);
+            sleep(1);
         }
         else {
             printf("Linked list is full. Waiting\n");
         }
     }
+    puts("Exiting Producer");
     pthread_exit(NULL);
 }
 
 void *Consumer(void* consumeEven) {
     int consumeEvenNumbers = (long) consumeEven;
-    int numCycles = 50;
+    int numCycles = 500;
     int curCycle;
     for (curCycle = 0; curCycle < numCycles; curCycle++) {
         if (numberOfNodes > 0) {
+            // Check if the list is locked
+            pthread_mutex_lock(&mutex);
+
+            puts("Consumer");
             removeNodeFromHead(consumeEvenNumbers);
             printf("Removing %s number from linked list, list has %d nodes.\n",
                 consumeEvenNumbers == 1 ? "even" : "odd",
                 numberOfNodes
             );
             printList();
-            sleep(3);
+            pthread_mutex_unlock(&mutex);
+            sleep(1);
         }
         else {
             printf("Linked list is empty. Waiting\n");
         }
     }
+    puts("Exiting Consumer");
     pthread_exit(NULL);
 }
 
@@ -144,15 +158,15 @@ int main() {
     srand(time(NULL));
     generateInitialValues();
     printList();
+    pthread_mutex_init(&mutex, NULL);
 
     pthread_t threads[4];
     int rc;
     long t;
     // Create producers
-    for(t = 0; t < 2; t++){
-        printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], NULL, Processor, (void *)t);
-        if (rc){
+    for (t = 0; t < 2; t++) {
+        rc = pthread_create(&threads[t], NULL, Producer, (void *)t);
+        if (rc) {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
@@ -160,7 +174,6 @@ int main() {
 
     // Create consumers
     for (t = 0; t < 2; t++) {
-        printf("In main: creating thread %ld\n", t + 2);
         rc = pthread_create(&threads[t + 2], NULL, Consumer, (void *) t);
         if (rc) {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
