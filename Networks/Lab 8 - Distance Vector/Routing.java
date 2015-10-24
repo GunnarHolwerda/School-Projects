fd71b2b58f4d904f29f95148e88c478f3344a070
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-10-24 16:33:53 -0600

    Adds comments

--- Networks/Lab 8 - Distance Vector/Routing.java
+++ Networks/Lab 8 - Distance Vector/Routing.java
@@ -11,6 +11,7 @@ class Routing {
 
     public Routing(String routerId) throws Exception {
         this.routerId = routerId;
+        // Initialize the hashmap for the current router
         configs.put(this.routerId, new HashMap<String, String>());
         this.setUpConfigMap();
         this.socket = new DatagramSocket(this.getPortNumber());
@@ -19,23 +20,51 @@ class Routing {
         this.printDistanceVector();
     }
 
+    /**
+        Returns the port number for the current router as an int
+
+        @return int
+    */
     public int getPortNumber() {
         return Integer.parseInt(this.configs.get(this.routerId).get("port"));
     }
 
+    /**
+        Initializes the configs Hashmap for the router
+    */
     public void setUpConfigMap() throws Exception {
         BufferedReader in = new BufferedReader(new FileReader(CONFIG_FILE_NAME));
         String line;
         String[] setting = {"port", "X", "Y", "Z"};
         int settingPosition = 0;
         while ((line = in.readLine()) != null) {
+            // Each string in the file is seperated by a tab
             String[] array = line.split("\t");
+            // The position in the array that we want is the current Char - X
             String myValue = array[routerId.charAt(0) - (int)'X'];
+            //TODO: Setup port number for other routers so that we can send them data
             this.configs.get(this.routerId).put(setting[settingPosition], myValue);
             settingPosition++;
         }
     }
 
+    /**
+        Starts the router waiting for updates and updates it's own tables
+        and sends updates
+    */
+    public void start() {
+        byte[] rcvData = new byte[1];
+        DatagramPacket rcvPkt = new DatagramPacket(rcvData, rcvData.length);
+        this.socket.receive(rcvPkt);
+
+        InetAddress senderIP = rcvPkt.getAddress();
+        int port = rcvPkt.getPort();
+        int value = (int) rcvPkt.getData()[0];
+    }
+
+    /**
+        Prints the distance vector for the current router
+    */
     public void printDistanceVector() {
         Iterator it = configs.get(this.routerId).entrySet().iterator();
         String printString = "";
@@ -60,6 +89,6 @@ class Routing {
         String routerId = consoleIn.next();
 
         Routing r = new Routing(routerId);
-
+        r.start();
     }
 }
