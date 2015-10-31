49a3b61ea352bc2a789ad5620b5b3a12f7d4c10f
Gunnar Holwerda <gunnarholwerda@gmail.com>
2015-10-30 21:23:58 -0600

    Breaks generation and stats to its own file, LOOK is finished

--- Operating Systems/DeviceManagement/device_management.py
+++ Operating Systems/DeviceManagement/device_management.py
@@ -4,58 +4,8 @@
     Written by: Gunnar Holwerda
 """
 
-import random
-import math
+from generation_and_calculation import *
 from pprint import pprint
-
-MAX_SECTOR = 8
-MAX_TRACK = 250
-
-
-def generate_test_data(num_of_requests):
-    """
-        Generates a random set of test requests for the program
-
-        :param num_of_requests: int, number of requests to create
-    """
-    arrival_times = []
-    cur_request = 0
-    # Generate list of random arrivale times
-    while cur_request < num_of_requests:
-        arrival_times.append(random.randint(0, 99))
-        cur_request += 1
-
-    arrival_times.sort()
-    test_data = []
-    cur_request = 0
-    while cur_request < num_of_requests:
-        test_data.append({
-            'arrival_time': arrival_times[cur_request],
-            'track': random.randint(0, MAX_TRACK - 1),
-            'sector': random.randint(0, MAX_SECTOR - 1)
-        })
-        cur_request += 1
-
-    return test_data
-
-
-def get_predetermined_test_data():
-    """
-        Returns the predetermined test data for the assignment
-    """
-    arrival_times = [0, 23, 26, 29, 35, 45, 57, 83, 88, 95]
-    track_requested = [54, 132, 29, 23, 198, 170, 180, 78, 73, 249]
-    sector_requested = [0, 6, 2, 1, 7, 5, 3, 4, 5, 7]
-
-    test_data = []
-    for i in range(len(arrival_times)):
-        test_data.append({
-            'arrival_time': arrival_times[i],
-            'track': track_requested[i],
-            'sector': sector_requested[i]
-        })
-
-    return test_data
 
 
 def calculate_seek_time(cur_position, new_position):
@@ -80,42 +30,57 @@ def calculate_seek_time(cur_position, new_position):
     return seek_time + sector_transfer_time
 
 
-def calculate_statistics(data):
+def get_closest_request(current_position, requests):
     """
-        Calculates the average, variance, and stdev for a series of data
+        Returns the closest request to the current position
 
-        :param data: list, a list of times to caluclate the stats for
+        :param cur_position: dict, dictionary outlining the current position
+        :param requests: list, list of requests to find the closest in
     """
-    total = 0
-    for value in data:
-        total += value
-    average = total / len(data)
+    closest_seek_time = 10000
+    closest_request = {}
+    for request in requests:
+        seek_time = calculate_seek_time(current_position, request)
 
-    total = 0
-    for value in data:
-        total += pow((value - average), 2)
-    variance = total / len(data)
+        if seek_time < closest_seek_time:
+            closest_seek_time = seek_time
+            closest_request = request
 
-    stdev = math.sqrt(variance)
-
-    return {
-        'average': average,
-        'variance': variance,
-        'stdev': stdev
-    }
+    # pprint(closest_request)
+    # print("\n")
+    return closest_request
 
 
-def print_statistics(time_series):
+def get_next_request_in_cur_direction(asc, current_position, requests):
     """
-        Prints out the statistics for the results
+        Returns the next job when we are traveling in direction
 
-        :param results: list, time series of turnaround times for an algorithm
+        :param asc: bool, true if the head is moving in ascending direction
+        :param current_position: dict, the current position of the head
+        :param requests: list, list of requests currently waiting
+
     """
-    stats = calculate_statistics(time_series)
-    print("Average: {0:.2f}".format(stats['average']))
-    print("Standard Deviation: {0:.2f}".format(stats['stdev']))
-    print("Variance: {0:.2f}".format(stats['variance']))
+    current_sector = current_position['sector']
+    current_track = current_position['track']
 
+    if not asc:
+        # Get all jobs with sectors lower than the current
+        waiting_requests = [i for i in requests if i[
+            'sector'] < current_sector or
+            (i['sector'] == current_sector and i['track'] <= current_track)]
+        #print("DESC")
+        #pprint(waiting_requests)
+        closest_request = get_closest_request(current_position, waiting_requests)
+    else:
+        # Get all jobs with sectors higher than the current
+        waiting_requests = [i for i in requests if i[
+            'sector'] > current_position['sector'] or
+            (i['sector'] == current_sector and i['track'] >= current_track)]
+        #print("ASC")
+        #pprint(waiting_requests)
+        closest_request = get_closest_request(current_position, waiting_requests)
+
+    return closest_request if closest_request else None
 
 def first_come_first_serve(test_data):
     """
@@ -159,17 +124,15 @@ def shortest_seek_time_first(test_data):
     time_data = []
     cur_time = 0
 
-    # Sort the data by sector
-    #test_data.sort(key=lambda t: t['sector'])
     while test_data:
-        #print("Time: {0:.2f}".format(cur_time))
+        # print("Time: {0:.2f}".format(cur_time))
 
         # Get all jobs that have arrived before the current_time
-        waiting_jobs = [i for i in test_data if i['arrival_time'] <= cur_time]
+        waiting_requests = [i for i in test_data if i['arrival_time'] <= cur_time]
 
-        if waiting_jobs:
+        if waiting_requests:
             # Get the closest request to this one
-            next_request = get_closest_request(current_position, waiting_jobs)
+            next_request = get_closest_request(current_position, waiting_requests)
             # pprint(next_request)
             test_data.remove(next_request)
             # Calcuatle the seek time to the closest request
@@ -179,41 +142,54 @@ def shortest_seek_time_first(test_data):
             turnaround_time = cur_time - next_request['arrival_time']
             time_data.append(turnaround_time)
             current_position = next_request
-
-        cur_time += 1
+        else:
+            cur_time += 1
 
     return time_data
 
 
-def get_closest_request(current_position, requests):
-    """
-        Returns the closest request to the current position
-
-        :param cur_position: dict, dictionary outlining the current position
-        :param requests: list, list of requests to find the closest in
-    """
-    closest_seek_time = 10000
-    closest_request = {}
-    for request in requests:
-        seek_time = calculate_seek_time(current_position, request)
-
-        if seek_time < closest_seek_time:
-            closest_seek_time = seek_time
-            closest_request = request
-
-    # pprint(closest_request)
-    # print("\n")
-    return closest_request
-
-
-def look(time_data):
+def look(test_data):
     """
         Runs the LOOK algorithm on the test data
 
         :param data: list, list of dictionaries that represent the requests
     """
-    # TODO: implement this method
-    pass
+    current_position = {
+        'track': 0,
+        'sector': 0
+    }
+    time_data = []
+    cur_time = 0
+    cur_direction = True
+
+    while test_data:
+        #print("TIME: {0:.2f}".format(cur_time))
+        # Get all jobs that have arrived before the current_time
+        waiting_requests = [i for i in test_data if i['arrival_time'] <= cur_time]
+
+        if waiting_requests:
+            next_request = get_next_request_in_cur_direction(
+                cur_direction, current_position, waiting_requests)
+
+            if not next_request:
+                cur_direction = not cur_direction
+                # We have nothing in the current direction, check the other direction
+                next_request = get_next_request_in_cur_direction(
+                    cur_direction, current_position, waiting_requests)
+
+            pprint(next_request)
+            test_data.remove(next_request)
+            # Calcuatle the seek time to the closest request
+            cur_seek_time = calculate_seek_time(current_position, next_request)
+
+            cur_time = cur_time + cur_seek_time
+            turnaround_time = cur_time - next_request['arrival_time']
+            time_data.append(turnaround_time)
+            current_position = next_request
+        else:
+            cur_time += 1
+
+    return time_data
 
 
 def clook(data):
@@ -227,13 +203,11 @@ def clook(data):
 
 TEST_REQUESTS = get_predetermined_test_data()
 print("********** FIRST COME FIRST SERVER  ****************")
-results = first_come_first_serve(TEST_REQUESTS)
-print_statistics(results)
+print_statistics(first_come_first_serve(TEST_REQUESTS))
 
 print("********** SHORTEST SEEK TIME FIRST ***************")
-results = shortest_seek_time_first(TEST_REQUESTS)
-print_statistics(results)
+print_statistics(shortest_seek_time_first(TEST_REQUESTS))
 
 print("**********          LOOK            ***************")
-results = look(TEST_REQUESTS)
-#print_statistics(results)
+TEST_REQUESTS = get_predetermined_test_data()
+print_statistics(look(TEST_REQUESTS))
---
+++ Operating Systems/DeviceManagement/generation_and_calculation.py
@@ -0,0 +1,91 @@
+"""
+    This file houses all functions to generate the tset data and also all functions to
+    calculate the stats on the time_series
+"""
+
+import random, math
+
+MAX_SECTOR = 8
+MAX_TRACK = 250
+
+
+def generate_test_data(num_of_requests):
+    """
+        Generates a random set of test requests for the program
+
+        :param num_of_requests: int, number of requests to create
+    """
+    arrival_times = []
+    cur_request = 0
+    # Generate list of random arrivale times
+    while cur_request < num_of_requests:
+        arrival_times.append(random.randint(0, 99))
+        cur_request += 1
+
+    arrival_times.sort()
+    test_data = []
+    cur_request = 0
+    while cur_request < num_of_requests:
+        test_data.append({
+            'arrival_time': arrival_times[cur_request],
+            'track': random.randint(0, MAX_TRACK - 1),
+            'sector': random.randint(0, MAX_SECTOR - 1)
+        })
+        cur_request += 1
+
+    return test_data
+
+
+def get_predetermined_test_data():
+    """
+        Returns the predetermined test data for the assignment
+    """
+    arrival_times = [0, 23, 26, 29, 35, 45, 57, 83, 88, 95]
+    track_requested = [54, 132, 29, 23, 198, 170, 180, 78, 73, 249]
+    sector_requested = [0, 6, 2, 1, 7, 5, 3, 4, 5, 7]
+
+    test_data = []
+    for i in range(len(arrival_times)):
+        test_data.append({
+            'arrival_time': arrival_times[i],
+            'track': track_requested[i],
+            'sector': sector_requested[i]
+        })
+
+    return test_data
+
+def calculate_statistics(data):
+    """
+        Calculates the average, variance, and stdev for a series of data
+
+        :param data: list, a list of times to caluclate the stats for
+    """
+    total=0
+    for value in data:
+        total += value
+    average=total / len(data)
+
+    total=0
+    for value in data:
+        total += pow((value - average), 2)
+    variance=total / len(data)
+
+    stdev=math.sqrt(variance)
+
+    return {
+        'average': average,
+        'variance': variance,
+        'stdev': stdev
+    }
+
+
+def print_statistics(time_series):
+    """
+        Prints out the statistics for the results
+
+        :param results: list, time series of turnaround times for an algorithm
+    """
+    stats=calculate_statistics(time_series)
+    print("Average: {0:.2f}".format(stats['average']))
+    print("Standard Deviation: {0:.2f}".format(stats['stdev']))
+    print("Variance: {0:.2f}".format(stats['variance']))
