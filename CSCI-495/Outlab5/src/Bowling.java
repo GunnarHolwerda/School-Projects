e94d9e7d103903e6746892363b74bafdc58e147f
Gunnar Holwerda <gunnarholwerda@gmail.com>
2016-03-29 17:43:44 -0600

    Everything working apart from printing final frame when it only has two rolls

--- CSCI-495/Outlab5/src/Bowling.java
+++ CSCI-495/Outlab5/src/Bowling.java
@@ -1,100 +1,129 @@
+import java.util.ArrayList;
+
 /**
  * Created by Gunnar on 3/22/2016.
  */
 public class Bowling {
 
     private int[] rolls;
-    private Frame[] frames;
     private int numberOfFrames, totalScore;
 
     Bowling(int[] rolls, int numberOfFrames) {
         this.rolls = rolls;
-        this.frames = new Frame[numberOfFrames];
         this.numberOfFrames = numberOfFrames;
-        buildFrames();
     }
 
-    private void buildFrames() {
-        Frame curFrame;
-        int frameCount = 1;
-        for (int i = 0; i < rolls.length && frameCount <= 10; i++) {
-            curFrame = new Frame(frameCount);
-
-            // Strike, set second frame to 10
-            if (rolls[i] == 10) {
-                curFrame.setRollTwo(rolls[i]);
-            }
-            // If this is the last roll, then it is a third role, add it as a third role to the previous frame
-            else if (i == rolls.length - 1) {
-                frames[frameCount - 1].setRollThree(rolls[i]);
-            }
-            // Normal frame
-            else {
-                curFrame.setRollOne(rolls[i]);
-                curFrame.setRollTwo(rolls[i + 1]);
-                i += 1;
-            }
-
-            frames[frameCount - 1] = curFrame;
-            frameCount++;
-        }
-    }
-
-    private void printFrame(int rollStart, int rollEnd) {
-        System.out.print("+");
+    private void printFrame(ArrayList<Integer> frame, int frameNumber) {
         String rollString = "|";
+        String topString = "+";
         String bottomString = "+";
-        for (int i = rollStart; i < rollEnd; i++) {
-            System.out.print("---+");
-            rollString += " " + rolls[i] + " |";
+        String bottomRollString = "|";
+        for (int i = 0; i < frame.size(); i++) {
+            topString += "---+";
+            if (frameIsStrike(frame)) {
+                topString += "---+";
+                rollString += "   |";
+                rollString += " X |";
+                bottomRollString += "---+";
+                bottomString += "----";
+            }
+            else if (frameIsSpare(frame) && i == frame.size() - 1) {
+                rollString += " / |";
+            }
+            else {
+                if (frame.get(i) == 10) {
+                    rollString += " X |";
+                }
+                else if (i >= 1 && frame.size() == 3 && frame.get(i - 1) + frame.get(i) == 10) {
+                    rollString += " / |";
+                }
+                else {
+                    rollString += " " + frame.get(i) + " |";
+                }
+            }
+            bottomRollString += "---+";
             bottomString += "----";
         }
-        System.out.println();
+        System.out.println(topString + "    Frame" + frameNumber);
         System.out.println(rollString);
-        System.out.printf("|%d|", totalScore);
-        System.out.println(bottomString + "+");
+        System.out.println(bottomRollString.substring(0, bottomRollString.length() - 1) + "|");
+        int spacesNeeded = bottomString.length() - 2;
+        String spaces = "";
+        for (int i = 0; i < spacesNeeded - (Integer.toString(totalScore) + " ").length(); i++) {
+            spaces += " ";
+        }
+        System.out.printf("|%s%d |\n", spaces, totalScore);
+        System.out.println(bottomString.substring(0, bottomString.length() - 1) + "+");
 
     }
 
-    private int calculateStrikeScore(int rollNumber) {
-        int nextRole = rollNumber + 1 > rolls.length ? 0 : rolls[rollNumber + 1];
-        int secondRole = rollNumber + 2 > rolls.length ? 0 : rolls[rollNumber + 2];
+    private int frameTotal(ArrayList<Integer> list) {
+        int total = 0;
+        for (Integer num: list) {
+            total += num;
+        }
 
-        return 10 + nextRole + secondRole;
+        return total;
     }
 
-    private int calculateSpareScore(int rollNumber) {
-        int nextRole = rollNumber + 1 > rolls.length ? 0 : rolls[rollNumber + 1];
-        int secondRole = rollNumber + 2 > rolls.length ? 0 : rolls[rollNumber + 2];
+    private void calculateScoreForFrame(ArrayList<Integer> frame, int currentRoll) {
+        if (frameIsStrike(frame)) {
+            if (currentRoll >= rolls.length - 2) {
+                totalScore += 10;
+            }
+            else {
+                totalScore += 10 + rolls[currentRoll + 1] + rolls[currentRoll + 2];
+            }
+        }
+        else if (frameIsSpare(frame)) {
+            if (currentRoll >= rolls.length - 2) {
+                totalScore += 10;
+            }
+            else {
+                totalScore += 10 + rolls[currentRoll + 1];
+            }
+        }
+        else {
+            totalScore += frameTotal(frame);
+        }
+    }
 
-        return 10 + nextRole + secondRole;
+    private boolean frameIsSpare(ArrayList<Integer> list) {
+        return list.size() == 2 && frameTotal(list) == 10;
+    }
+
+    private boolean frameIsStrike(ArrayList<Integer> list) {
+        return list.size() == 1 && frameTotal(list) == 10;
     }
 
     public void play() {
-        System.out.println("GAME!");
-        int frame = 1;
-//        while (frame <= numberOfFrames) {
-//            for (int i = 0; i < rolls.length; i++) {
-//
-//                //Strike scenario
-//                if (rolls[i] == 10) {
-//                    totalScore += calculateStrikeScore(i);
-//                    frame++;
-//                    printFrame(i, i);
-//                }
-//                else if ((rolls[i] + rolls[i + 1]) == 10) {
-//                    totalScore += calculateSpareScore(i);
-//                    frame++;
-//                    printFrame(i, i + 1);
-//                }
-//                else {
-//
-//                }
-//            }
-//        }
-//        for (int roll: frames) {
-//            totalScore += calculateScore(frame);
-//            printFrame(frame);
-//        }
+        ArrayList<Integer> currentFrame = new ArrayList<Integer>();
+        int frameNumber = 0;
+        for (int currentRoll = 0; currentRoll < rolls.length; currentRoll++) {
+            currentFrame.add(rolls[currentRoll]);
+
+            // Check to get the 3 roll frame
+            if (currentRoll == rolls.length - 3 && currentFrame.size() == 1) {
+                frameNumber++;
+                currentFrame.add(rolls[rolls.length - 2]);
+                currentFrame.add(rolls[rolls.length - 1]);
+                calculateScoreForFrame(currentFrame, currentRoll);
+                printFrame(currentFrame, frameNumber);
+                currentFrame.clear();
+                break;
+            }
+            else if (frameTotal(currentFrame) == 10) {
+                frameNumber++;
+                calculateScoreForFrame(currentFrame, currentRoll);
+                printFrame(currentFrame, frameNumber);
+                currentFrame.clear();
+            }
+            else if(currentFrame.size() == 2 && currentRoll < rolls.length - 1) {
+                frameNumber++;
+                calculateScoreForFrame(currentFrame, currentRoll);
+                printFrame(currentFrame, frameNumber);
+                currentFrame.clear();
+            }
+        }
     }
 }
